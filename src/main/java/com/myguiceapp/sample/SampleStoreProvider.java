9da38e22f497d329cb4c8d42f3093516c6d0bdce
Ryan Catlin <ryan.catlin@gmail.com>
2018-03-14 23:57:59 -0400

    Remove unused SampleStoreProvider

--- src/main/java/com/myguiceapp/sample/SampleStoreProvider.java
+++
@@ -1,9 +0,0 @@
-package com.myguiceapp.sample;
-
-import com.google.inject.Provider;
-
-public class SampleStoreProvider implements Provider<SampleStore> {
-    public SampleStore get() {
-        return new SampleStore();
-    }
-}
